aebefa6983b4ffb34683cacc9ca4e4ccf3764d82
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2026-03-14 23:11:21 +0600

    Setting part design

--- client/src/app/(root)/layout.jsx
+++ client/src/app/(root)/layout.jsx
@@ -1,16 +1,17 @@
-"use client"
+"use client";
 import Header from "@/components/common/Header";
 import ProfileSidebar from "@/components/common/ProfileSidebar";
+import Settingsbar from "@/components/common/Settingsbar";
 
 export default function MainLayout({ children }) {
   return (
     <main>
-      <Header/>
-      <div className="grid grid-cols-3 bg-[#F5F5F5]">
-        <ProfileSidebar/>
-      {children}
+      <Header />
+      <div className="flex justify-between bg-[#F5F5F5]">
+        <ProfileSidebar />
+        {children}
+        <Settingsbar />
       </div>
-      
     </main>
   );
 }
---
+++ client/src/components/common/Settingsbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Settingsbar = () => {
+  return (
+    <div className='w-20 bg-white'>
+        <div className='settingsbar'></div>
+        <div className='settingsbar'></div>
+        <div className='settingsbar'></div>
+        <div className='settingsbar'></div>
+        <div className='settingsbar'></div>
+        <div className='settingsbar'></div>
+        <div className='settingsbar'></div>
+        <div className='settingsbar'></div>
+        <div className='settingsbar'></div>
+        <div className='settingsbar'></div>
+    </div>
+  )
+}
+
+export default Settingsbar
