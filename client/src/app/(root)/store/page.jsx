33355f265a30e550dfafc5485afc514447d280a7
ayeshaakter02 <ayeshaakter0123456789@gmail.com>
2026-03-22 22:20:15 +0600

    create a store page & marketPlace component design that is reuseable

---
+++ client/src/app/(root)/store/page.jsx
@@ -0,0 +1,12 @@
+import MarketPlace from '@/components/common/MarketPlace'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <MarketPlace/>
+    </div>
+  )
+}
+
+export default page
---
+++ client/src/components/common/MarketPlace.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const MarketPlace = () => {
+  return (
+    <div className="flex w-296 bg-[#615DFA] gap-9 items-center mt-9 ml-9 rounded-[14px]">
+        <img src="./images/market-place.png" alt="" />
+        <div className='font-poppins text-white'>
+            <h2 className='font-bold text-[36px]'>MERNIAN Marketplace</h2>
+            <p className='font-normal text-[15px]'>The best place for the community to buy and sell!</p>
+        </div>
+    </div>
+  )
+}
+
+export default MarketPlace
